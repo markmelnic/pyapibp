4bd2678ee374a26e6f50ed7edff310092485501b
markmelnic <markhappy200@gmail.com>
2021-09-10 17:00:11 +0300

    Entry point added and style adjusted

--- pyapibp/_style.py
+++ pyapibp/_style.py
@@ -4,6 +4,6 @@ STYLE = style_from_dict({
     Token.QuestionMark: '#E91E63 bold',
     Token.Selected: '#673AB7 bold',
     Token.Instruction: '',
-    Token.Answer: '#2196f3 bold',
-    Token.Question: '#ffb333 bold',
+    Token.Answer: '#2196F3',
+    Token.Question: '#FFB333',
 })
